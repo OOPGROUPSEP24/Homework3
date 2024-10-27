acf4f1c4841220b24353ce8fbab79c0daa40791a
OOPGROUPSEP24 <adrianayusrii204@gmail.com>
2024-10-28 01:05:53 +0800

    balloonpurple.cs

---
+++ balloonpurple.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class balloonpurple : MonoBehaviour
+{
+    public float moveSpeed = 2f; // Speed at which the balloon moves
+
+    void Update()
+    {
+        // Move the balloon upwards
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+        // Destroy the balloon once it goes off the screen
+        if (transform.position.y > Camera.main.orthographicSize + 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
